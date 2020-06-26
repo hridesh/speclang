d6257d4cb041b79ae2fcb710fbb811f7da4f1289
Hridesh Rajan <hridesh@iastate.edu>
2020-06-26 13:52:16 -0500

    make consistent with book

--- src/speclang/examples/divbyzero.scm
+++ src/speclang/examples/divbyzero.scm
@@ -1,8 +1,9 @@
-(define f : (num -> num)
-	(lambda 
-		(x:num | (> x 0) -> (> result 0)) 
-		(/ 342 x)
-	)
+(let 
+	((f : (num -> num)
+		(lambda 
+			(x:num | (> x 0) -> (> result 0)) 
+			(/ 342 x)
+		)
+	))
+	(f 0) //Precondition violation here.
 )
-
-(f 0) //Precondition violation here.
