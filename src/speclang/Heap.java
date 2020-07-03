a661299ee9ad3abfd8f3b45a6763489a4c71c0dc
Hridesh Rajan <hridesh@iastate.edu>
2020-07-03 08:25:36 -0500

    Handle Program Errors

--- src/speclang/Heap.java
+++ src/speclang/Heap.java
@@ -1,5 +1,7 @@
 package speclang;
 
+import speclang.AST.ProgramError;
+
 /**
  * Representation of a heap, which maps references to values.
  * 
@@ -8,13 +10,13 @@ package speclang;
  */
 public interface Heap {
 
-	Value ref(Value value);
+	Value ref(Value value) throws ProgramError;
 
-	Value deref(Value.RefVal loc);
+	Value deref(Value.RefVal loc) throws ProgramError;
 
-	Value setref(Value.RefVal loc, Value value);
+	Value setref(Value.RefVal loc, Value value) throws ProgramError;
 
-	Value free(Value.RefVal value);
+	Value free(Value.RefVal value) throws ProgramError;
 
 	static public class Heap16Bit implements Heap {
 		static final int HEAP_SIZE = 65_536;
@@ -22,36 +24,36 @@ public interface Heap {
 		Value[] _rep = new Value[HEAP_SIZE];
 		int index = 0;
 
-		public Value ref(Value value) {
+		public Value ref(Value value) throws ProgramError {
 			if (index >= HEAP_SIZE)
-				return new Value.DynamicError("Out of memory error");
+				throw new ProgramError("Out of memory error");
 			Value.RefVal new_loc = new Value.RefVal(index);
 			_rep[index++] = value;
 			return new_loc;
 		}
 
-		public Value deref(Value.RefVal loc) {
+		public Value deref(Value.RefVal loc) throws ProgramError {
 			try {
 				return _rep[loc.loc()];
 			} catch (ArrayIndexOutOfBoundsException e) {
-				return new Value.DynamicError("Segmentation fault at access " + loc);
+				throw new ProgramError("Segmentation fault at access " + loc);
 			}
 		}
 
-		public Value setref(Value.RefVal loc, Value value) {
+		public Value setref(Value.RefVal loc, Value value) throws ProgramError {
 			try {
 				return _rep[loc.loc()] = value;
 			} catch (ArrayIndexOutOfBoundsException e) {
-				return new Value.DynamicError("Segmentation fault at access " + loc);
+				throw new ProgramError("Segmentation fault at access " + loc);
 			}
 		}
 
-		public Value free(Value.RefVal loc) {
+		public Value free(Value.RefVal loc) throws ProgramError {
 			try {
 				_rep[loc.loc()] = null;
 				return loc;
 			} catch (ArrayIndexOutOfBoundsException e) {
-				return new Value.DynamicError("Segmentation fault at access " + loc);
+				throw new ProgramError("Segmentation fault at access " + loc);
 			}
 		}
 
